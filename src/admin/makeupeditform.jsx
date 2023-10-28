e0b839de5f3e668aed1b1e305328254e3bdc64da
Hastinul Chotimah <142318032+hastinulchotimah@users.noreply.github.com>
2023-10-28 22:29:50 +0700

    perubahan

--- src/admin/makeupeditform.jsx
+++ src/admin/makeupeditform.jsx
@@ -1,23 +1,18 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 
+function EditBookingForm(props) {
+  const [formData, setFormData] = useState({
+    name: '',
+    instagram: '',
+    alamat: '',
+    telepon: '',
+    appointmentDate: '',
+    serviceType: '',
+    message: '',
+  });
 
-class EditBookingForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      instagram: "",
-      alamat: "",
-      telepon: "",
-      appointmentDate: "",
-      serviceType: "",
-      message: "",
-    };
-  }
-
-  componentDidMount() {
-    const { booking } = this.props;
-    if (booking) {
+  useEffect(() => {
+    if (props.booking) {
       const {
         id,
         name,
@@ -27,8 +22,8 @@ class EditBookingForm extends Component {
         appointmentDate,
         serviceType,
         message,
-      } = booking;
-      this.setState({
+      } = props.booking;
+      setFormData({
         id,
         name,
         instagram,
@@ -39,130 +34,124 @@ class EditBookingForm extends Component {
         message,
       });
     }
-  }
+  }, [props.booking]);
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    setFormData({ ...formData, [name]: value });
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const updatedBooking = { ...this.state };
-    this.props.onSave(updatedBooking);
+    props.onSave({ ...formData });
   };
 
-  handleCancel = () => {
-    this.props.onCancel();
+  const handleCancel = () => {
+    props.onCancel();
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="mb-4">
-          <label className="mb-2 block text-sm font-bold text-gray-700">
-            Nama:
-          </label>
-          <input
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="mb-2 block text-sm font-bold text-gray-700">
-            Nama Instagram:
-          </label>
-          <input
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            type="text"
-            name="instagram"
-            value={this.state.instagram}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="mb-2 block text-sm font-bold text-gray-700">
-            Alamat:
-          </label>
-          <input
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            type="text"
-            name="alamat"
-            value={this.state.alamat}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="mb-2 block text-sm font-bold text-gray-700">
-            No. Telepon:
-          </label>
-          <input
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            type="text"
-            name="telepon"
-            value={this.state.telepon}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="mb-2 block text-sm font-bold text-gray-700">
-            Tanggal Appointment:
-          </label>
-          <input
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            type="text"
-            name="appointmentDate"
-            value={this.state.appointmentDate}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="mb-2 block text-sm font-bold text-gray-700">
-            Jenis Layanan:
-          </label>
-          <input
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            type="text"
-            name="serviceType"
-            value={this.state.serviceType}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="mb-2 block text-sm font-bold text-gray-700">
-            Pesan Tambahan:
-          </label>
-          <textarea
-            className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
-            name="message"
-            value={this.state.message}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="mb-4">
-          <button
-            className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
-            type="submit"
-          >
-            Simpan
-          </button>
-          <button
-            className="focus:shadow-outline ml-2 rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700 focus:outline-none"
-            type="button"
-            onClick={this.handleCancel}
-          >
-            Batal
-          </button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Nama:
+        </label>
+        <input
+          className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Nama Instagram:
+        </label>
+        <input
+          className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          type="text"
+          name="instagram"
+          value={formData.instagram}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Alamat:
+        </label>
+        <input
+          className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          type="text"
+          name="alamat"
+          value={formData.alamat}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          No. Telepon:
+        </label>
+        <input
+          className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          type="text"
+          name="telepon"
+          value={formData.telepon}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Tanggal Appointment:
+        </label>
+        <input
+          className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          type="text"
+          name="appointmentDate"
+          value={formData.appointmentDate}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Jenis Layanan:
+        </label>
+        <input
+          className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          type="text"
+          name="serviceType"
+          value={formData.serviceType}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="mb-2 block text-sm font-bold text-gray-700">
+          Pesan Tambahan:
+        </label>
+        <textarea
+          className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4">
+        <button
+          className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+          type="submit"
+        >
+          Simpan
+        </button>
+        <button
+          className="focus:shadow-outline ml-2 rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700 focus:outline-none"
+          type="button"
+          onClick={handleCancel}
+        >
+          Batal
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default EditBookingForm;
-
-
-
--- src/pages/formBooking.jsx
+++ src/pages/formBooking.jsx
@@ -1,62 +1,60 @@
-import React, { Component } from 'react';
-import Navbar from "../components/navbar";
+import React, { useState, useEffect, useRef } from 'react';
+import Navbar from '../components/navbar';
+import backgroundImage from './booking.avif';
 import axios from 'axios';
-import backgroundImage from "./booking.avif";
 
-class MakeupBookingForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      instagram: '',
-      alamat: '',
-      telepon: '',
-      appointmentDate: '',
-      serviceType: '',
-      message: '',
-      bookings: [],
-      currentBooking: null,
-      isEditMode: false,
-      successMessage: '',
-    };
-    this.makeupListRef = React.createRef();
-  }
+function MakeupBookingForm(props) {
+  const [formData, setFormData] = useState({
+    name: '',
+    instagram: '',
+    alamat: '',
+    telepon: '',
+    appointmentDate: '',
+    serviceType: '',
+    message: '',
+    bookings: [],
+    currentBooking: null,
+    isEditMode: false,
+    successMessage: '',
+  });
 
-  componentDidMount() {
-    this.loadBookings();
-  }
+  const makeupListRef = useRef(null);
 
-  loadBookings() {
+  useEffect(() => {
+    loadBookings();
+  }, []);
+
+  const loadBookings = () => {
     axios.get('https://651b95ed194f77f2a5ae9790.mockapi.io/makeup')
       .then((response) => {
-        this.setState({ bookings: response.data });
+        setFormData({ ...formData, bookings: response.data });
       })
       .catch((error) => {
         console.error('Error memuat data pemesanan:', error);
       });
-  }
+  };
 
-  handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
+    setFormData({ ...formData, [name]: value });
+  };
 
-  handleEditSubmit = (e) => {
-    
+  const handleEditSubmit = (e) => {
     e.preventDefault();
-    const { bookings, currentBooking } = this.state;
+    const { bookings, currentBooking } = formData;
     const editedBookingIndex = bookings.findIndex((booking) => booking.id === currentBooking.id);
 
     if (editedBookingIndex !== -1) {
       const updatedBookings = [...bookings];
       updatedBookings[editedBookingIndex] = currentBooking;
 
-      this.updateBookings(updatedBookings);
+      updateBookings(updatedBookings);
     }
-  }
-  
-  resetForm = () => {
-    this.setState({
+  };
+
+  const resetForm = () => {
+    setFormData({
+      ...formData,
       name: '',
       instagram: '',
       alamat: '',
@@ -67,10 +65,10 @@ class MakeupBookingForm extends Component {
       isEditMode: false,
       currentBooking: null,
     });
-    this.loadBookings(); 
-  }
+    loadBookings();
+  };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const {
       name,
@@ -82,28 +80,33 @@ class MakeupBookingForm extends Component {
       message,
       isEditMode,
       currentBooking,
-    } = this.state;
-  
-    const dataToSubmit = isEditMode ? currentBooking : {
-      name,
-      instagram,
-      alamat,
-      telepon,
-      appointmentDate,
-      serviceType,
-      message,
-    };
-  
+    } = formData;
+
+    const dataToSubmit = isEditMode
+      ? currentBooking
+      : {
+          name,
+          instagram,
+          alamat,
+          telepon,
+          appointmentDate,
+          serviceType,
+          message,
+        };
+
     if (isEditMode) {
       axios
         .put(`https://651b95ed194f77f2a5ae9790.mockapi.io/makeup/${currentBooking.id}`, dataToSubmit)
         .then((response) => {
-          
-          const updatedBookings = this.state.bookings.map((booking) =>
+          const updatedBookings = formData.bookings.map((booking) =>
             booking.id === currentBooking.id ? response.data : booking
           );
-          this.setState({ bookings: updatedBookings });
-          this.resetForm();
+          setFormData({
+            ...formData,
+            bookings: updatedBookings,
+            successMessage: 'Pemesanan berhasil dikirim!',
+          });
+          resetForm();
         })
         .catch((error) => {
           console.error('Error mengupdate pemesanan:', error);
@@ -112,42 +115,44 @@ class MakeupBookingForm extends Component {
       axios
         .post('https://651b95ed194f77f2a5ae9790.mockapi.io/makeup', dataToSubmit)
         .then((response) => {
-          this.setState((prevState) => ({
+          setFormData((prevState) => ({
+            ...formData,
             bookings: [...prevState.bookings, response.data],
-            successMessage: 'Pemesanan berhasil dikirim!', 
+            successMessage: 'Pemesanan berhasil dikirim!',
           }));
-          this.resetForm();
-          this.loadBookings();
+          resetForm();
         })
         .catch((error) => {
           console.error('Error menyimpan pemesanan:', error);
         });
     }
-  }
+  };
 
-  
-  
-  updateBookings = (updatedBookings) => {
-    this.setState({ bookings: updatedBookings });
-    this.makeupListRef.current.loadBookingsFromAPI();
-    this.resetForm();
-  }
+  const updateBookings = (updatedBookings) => {
+    setFormData({ ...formData, bookings: updatedBookings });
+    if (makeupListRef.current) {
+      makeupListRef.current.loadBookings();
+    }
+    resetForm();
+  };
 
-  deleteBooking = (id) => {
-    axios.delete(`https://651b95ed194f77f2a5ae9790.mockapi.io/makeup/${id}`)
+  const deleteBooking = (id) => {
+    axios
+      .delete(`https://651b95ed194f77f2a5ae9790.mockapi.io/makeup/${id}`)
       .then(() => {
-        this.loadBookings();
+        loadBookings();
       })
       .catch((error) => {
         console.error('Error menghapus pemesanan:', error);
       });
-  }
+  };
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.editingBooking !== this.props.editingBooking) {
-      const { editingBooking } = this.props;
+  useEffect(() => {
+    if (props.editingBooking !== formData.currentBooking) {
+      const { editingBooking } = props;
       if (editingBooking) {
-        this.setState({
+        setFormData({
+          ...formData,
           name: editingBooking.name,
           instagram: editingBooking.instagram,
           alamat: editingBooking.alamat,
@@ -157,116 +162,117 @@ class MakeupBookingForm extends Component {
           message: editingBooking.message,
           isEditMode: true,
           currentBooking: editingBooking,
+          successMessage: '', 
         });
       }
     }
-  }
- 
+  }, [props.editingBooking, formData]);
 
-  render() {
-    const backgroundStyle = {
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      height: "110vh",
-    };
-    return (
-      <div>
-        <Navbar />
-          <div className="mx-auto p-9 rounded-lg shadow-md opacity-90" style={backgroundStyle}>
-            <h2 className="text-2xl font-semibold mb-4 text-center text-black">Booking Jasa Makeup</h2>
-            {this.state.successMessage && (
-              <div className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4">
-                {this.state.successMessage}
-              </div>
-            )}
-            <form onSubmit={this.handleSubmit}>
-              <div className="">
-                <label className="block text-black font-semibold">Nama:</label>
-                <input
-                  className="border border-grey-300 rounded-lg p-2 w-full"
-                  type="text"
-                  name="name"
-                  value={this.state.isEditMode ? this.state.currentBooking.name : this.state.name}
-                  onChange={this.handleInputChange}
-                  required
-                />
-              </div>
-              <div className="">
-                <label className="block text-black font-semibold">Nama Instagram:</label>
-                <input
-                  className="border border-gray-300 rounded-lg p-2 w-full"
-                  type="text"
-                  name="instagram"
-                  value={this.state.isEditMode ? this.state.currentBooking.instagram : this.state.instagram}
-                  onChange={this.handleInputChange}
-                  required
-                />
-              </div>
-              <div className="">
-                <label className="block text-black font-semibold">Alamat:</label>
-                <input
-                  className="border border-gray-300 rounded-lg p-2 w-full"
-                  type="text"
-                  name="alamat"
-                  value={this.state.isEditMode ? this.state.currentBooking.alamat : this.state.alamat}
-                  onChange={this.handleInputChange}
-                  required
-                />
-              </div>
-              <div className="">
-                <label className="block text-black font-semibold">No.Telp:</label>
-                <input
-                  className="border border-gray-300 rounded-lg p-2 w-full"
-                  type="text"
-                  name="telepon"
-                  value={this.state.isEditMode ? this.state.currentBooking.telepon : this.state.telepon}
-                  onChange={this.handleInputChange}
-                  required
-                />
-              </div>
-              <div className="">
-                <label className="block text-black font-semibold">Tanggal Appointment:</label>
-                <input
-                  className="border border-gray-300 rounded-lg p-2 w-full"
-                  type="date"
-                  name="appointmentDate"
-                  value={this.state.isEditMode ? this.state.currentBooking.appointmentDate : this.state.appointmentDate}
-                  onChange={this.handleInputChange}
-                  required
-                />
-              </div>
-              <div className="">
-                <label className="block text-black font-semibold">Jenis Layanan:</label>
-                <select
-                  className="border border-gray-300 rounded-lg p-2 w-full"
-                  name="serviceType"
-                  value={this.state.isEditMode ? this.state.currentBooking.serviceType : this.state.serviceType}
-                  onChange={this.handleInputChange}
-                  required
-                >
-                  <option value="">Pilih Jenis Layanan</option>
-                  <option value="Makeup Pesta">Makeup Pesta</option>
-                  <option value="Makeup Pengantin">Makeup Pengantin</option>
-                  <option value="Makeup Wisuda">Makeup Wisuda</option>
-                </select>
-              </div>
-              <div className="">
-                <label className="block text-balck font-semibold">Pesan Tambahan:</label>
-                <textarea
-                  className="border border-gray-300 rounded-lg p-2 w-full"
-                  type="text"
-                  name="message"
-                  value={this.state.isEditMode ? this.state.currentBooking.message : this.state.message}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-              <button className="bg-blue-600 text-white rounded-lg p-2 hover:bg-blue-700" type="submit">Submit</button>
-            </form>
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '110vh',
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="mx-auto p-9 rounded-lg shadow-md opacity-90" style={backgroundStyle}>
+        <h2 className="text-2xl font-semibold mb-4 text-center text-black">Booking Jasa Makeup</h2>
+        {formData.successMessage && (
+          <div className="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4">
+            {formData.successMessage}
           </div>
-        </div>
-    );
-  }
+        )}
+        <form onSubmit={handleSubmit}>
+          <div className="">
+            <label className="block text-black font-semibold">Nama:</label>
+            <input
+              className="border border-grey-300 rounded-lg p-2 w-full"
+              type="text"
+              name="name"
+              value={formData.isEditMode ? formData.currentBooking.name : formData.name}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="block text-black font-semibold">Nama Instagram:</label>
+            <input
+              className="border border-gray-300 rounded-lg p-2 w-full"
+              type="text"
+              name="instagram"
+              value={formData.isEditMode ? formData.currentBooking.instagram : formData.instagram}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="block text-black font-semibold">Alamat:</label>
+            <input
+              className="border border-gray-300 rounded-lg p-2 w-full"
+              type="text"
+              name="alamat"
+              value={formData.isEditMode ? formData.currentBooking.alamat : formData.alamat}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="block text-black font-semibold">No.Telp:</label>
+            <input
+              className="border border-gray-300 rounded-lg p-2 w-full"
+              type="text"
+              name="telepon"
+              value={formData.isEditMode ? formData.currentBooking.telepon : formData.telepon}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="block text-black font-semibold">Tanggal Appointment:</label>
+            <input
+              className="border border-gray-300 rounded-lg p-2 w-full"
+              type="date"
+              name="appointmentDate"
+              value={formData.isEditMode ? formData.currentBooking.appointmentDate : formData.appointmentDate}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="">
+            <label className="block text-black font-semibold">Jenis Layanan:</label>
+            <select
+              className="border border-gray-300 rounded-lg p-2 w-full"
+              name="serviceType"
+              value={formData.isEditMode ? formData.currentBooking.serviceType : formData.serviceType}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Pilih Jenis Layanan</option>
+              <option value="Makeup Pesta">Makeup Pesta</option>
+              <option value="Makeup Pengantin">Makeup Pengantin</option>
+              <option value="Makeup Wisuda">Makeup Wisuda</option>
+            </select>
+          </div>
+          <div className="">
+            <label className="block text-balck font-semibold">Pesan Tambahan:</label>
+            <textarea
+              className="border border-gray-300 rounded-lg p-2 w-full"
+              type="text"
+              name="message"
+              value={formData.isEditMode ? formData.currentBooking.message : formData.message}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button className="bg-blue-600 text-white rounded-lg p-2 hover-bg-blue-700" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default MakeupBookingForm;
