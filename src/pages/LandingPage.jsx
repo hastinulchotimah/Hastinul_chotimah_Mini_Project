7945b110459f5e84a49f0583b567c704732db8a1
Hastinul Chotimah <142318032+hastinulchotimah@users.noreply.github.com>
2023-10-29 13:04:44 +0700

    penambahan use navigate

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,12 @@
-
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/copyright";
 import Navbar from "../components/navbar";
 import backgroundImage from "./booking.avif";
 
 export default function LandingPage() {
+  const navigate = useNavigate(); 
+
   const backgroundStyle = {
     backgroundImage: `url(${backgroundImage})`,
     backgroundSize: "cover",
@@ -13,38 +14,38 @@ export default function LandingPage() {
   };
 
   const handleBookNowClick = () => {
-    alert("Selamat datang di Form Booking!"); 
+    alert("Selamat datang di Form Booking!");
+    navigate("/booking"); 
   };
 
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
-        <div className="bg-pink-800 text-white flex-grow p-40 text-center flex flex-col items-center sm:flex-row sm:justify-between" style={backgroundStyle}>
+      <div className="bg-pink-800 text-white flex-grow p-40 text-center flex flex-col items-center sm:flex-row sm:justify-between" style={backgroundStyle}>
         <div className="text-center sm:text-left">
-            <h1 className="text-4xl font-bold mb-4">
+          <h1 className="text-4xl font-bold mb-4">
             Welcome To our  <br /> MakeUp Artist Website
           </h1>
           <p>where beauty meets you. Discover the secrets to looking stunning with us.</p>
-         <br />
-         <div className="space-y-1 flex">
-            <Link to="/booking" onClick={handleBookNowClick } className="rounded-full bg-white px-4 py-2 text-center font-bold text-black hover:bg-pink-600">
+          <br />
+          <div className="space-y-1 flex">
+            <button onClick={handleBookNowClick} className="rounded-full bg-white px-4 py-2 text-center font-bold text-black hover:bg-pink-600">
               Book Now
-            </Link>  
+            </button>
             <Link to="/galery" className="rounded-full border border-pink-200 px-4 py-2 text-center font-bold text-white hover:bg-pink-900">
-            Gallery
-          </Link>
+              Gallery
+            </Link>
           </div>
         </div>
-            <img
-              src="page.avif"
-              alt="gambar"
-              width="500"
-              height="400"
-              className="mt-8  border border-gray-900 rounded-md "
-            />
-          </div>
-          <Footer/>
+        <img
+          src="page.avif"
+          alt="gambar"
+          width="500"
+          height="400"
+          className="mt-8  border border-gray-900 rounded-md "
+        />
+      </div>
+      <Footer/>
     </div>
-   
   );
 }
